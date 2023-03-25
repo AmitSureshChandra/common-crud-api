515553fd47b811ed26496271184385e6f8464873
Amit <akumar00029@gmail.com>
2023-03-25 22:16:51 +0530

    project created

---
+++ src/test/java/com/github/amitsureshchandra/commonfeature/CommonFeatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.amitsureshchandra.commonfeature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonFeatureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
