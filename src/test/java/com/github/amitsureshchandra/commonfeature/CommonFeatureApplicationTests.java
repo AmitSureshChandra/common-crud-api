a9919cdebd5e715f576713752a0dd9e7b0382549
Amit <akumar00029@gmail.com>
2023-03-26 18:25:26 +0530

    working

--- src/test/java/com/github/amitsureshchandra/commonfeature/CommonFeatureApplicationTests.java
+++ src/test/java/com/github/amitsureshchandra/commonfeature/CommonFeatureApplicationTests.java
@@ -1,13 +1,5 @@
 package com.github.amitsureshchandra.commonfeature;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class CommonFeatureApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
