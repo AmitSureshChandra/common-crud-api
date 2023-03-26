a89e0e313629fadb877646b46a36b6eaca623a9e
Amit <akumar00029@gmail.com>
2023-03-26 17:42:08 +0530

    as jar added

---
+++ src/main/java/com/github/amitsureshchandra/commonfeature/service/feature/IGetAppContext.java
@@ -0,0 +1,6 @@
+package com.github.amitsureshchandra.commonfeature.service.feature;
+
+import org.springframework.context.ApplicationContext;
+public interface IGetAppContext {
+    ApplicationContext getAppContext();
+}
