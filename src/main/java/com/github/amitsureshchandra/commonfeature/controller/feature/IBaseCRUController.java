a2d47d291ed251589d8000729d0a64861893368c
Amit <akumar00029@gmail.com>
2023-03-28 20:41:33 +0530

    update working

--- src/main/java/com/github/amitsureshchandra/commonfeature/controller/feature/IBaseCRUController.java
+++ src/main/java/com/github/amitsureshchandra/commonfeature/controller/feature/IBaseCRUController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 public interface IBaseCRUController<T, T_ID, T_CREATE_DTO, T_UPDATE_DTO> {
@@ -21,12 +22,12 @@ public interface IBaseCRUController<T, T_ID, T_CREATE_DTO, T_UPDATE_DTO> {
     }
 
     @PostMapping
-    default ResponseEntity<T> create(@RequestBody T_CREATE_DTO createDto){
+    default ResponseEntity<T> create(@RequestBody @Valid T_CREATE_DTO createDto){
         return new ResponseEntity<>(getCRUService().create(createDto), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    default ResponseEntity<T> getById(@PathVariable T_ID id, @RequestBody T_UPDATE_DTO updateDto) {
+    default ResponseEntity<T> getById(@PathVariable T_ID id, @RequestBody @Valid T_UPDATE_DTO updateDto) {
         return new ResponseEntity<>(getCRUService().update(id, updateDto), HttpStatus.OK);
     }
 }
--- src/main/java/com/github/amitsureshchandra/commonfeature/service/feature/IBaseCRUService.java
+++ src/main/java/com/github/amitsureshchandra/commonfeature/service/feature/IBaseCRUService.java
@@ -2,12 +2,18 @@ package com.github.amitsureshchandra.commonfeature.service.feature;
 
 import com.github.amitsureshchandra.commonfeature.exception.ValidationException;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public interface IBaseCRUService<T, T_ID, T_CREATE_DTO, T_UPDATE_DTO> extends IGetRepo<T, T_ID>, IGetAppContext {
+
+    Logger logger = LoggerFactory.getLogger(IBaseCRUService.class);
     default T findById(T_ID id){
         return getRepo().findById(id).orElseThrow(() -> new ValidationException("not found"));
     }
@@ -16,24 +22,92 @@ public interface IBaseCRUService<T, T_ID, T_CREATE_DTO, T_UPDATE_DTO> extends IG
         return getRepo().findAll();
     }
 
-    default T beforeCreate(T entity, T_CREATE_DTO dto) {
-        return entity;
+    default void beforeCreate(T entity, T_CREATE_DTO dto) {
+        return ;
     }
 
     default T create(T_CREATE_DTO dto) {
-        Type type = ((Class<T>) ((ParameterizedType) getClass().getGenericInterfaces()[0]).getActualTypeArguments()[0]);
+        Type type = getEntityType();
         T entity = ((ModelMapper)(getAppContext().getBean("modelMapper"))).map(dto, type);
-        entity = beforeCreate(entity, dto);
+        beforeCreate(entity, dto);
         return getRepo().save(entity);
+    }
+
+    default Type getEntityType() {
+        return ((Class<T>) ((ParameterizedType) getClass().getGenericInterfaces()[0]).getActualTypeArguments()[0]);
+    }
+
+    default Type getUpdateEntityType() {
+        return ((Class<T>) ((ParameterizedType) getClass().getGenericInterfaces()[0]).getActualTypeArguments()[3]);
     }
 
     default T update(T_ID id, T_UPDATE_DTO updateDto) {
         T entity = findById(id);
-        entity = beforeUpdate(entity, updateDto);
+        updateFields(entity, updateDto);
+        beforeUpdate(entity, updateDto);
         return getRepo().save(entity);
     }
 
-    default T beforeUpdate(T entity, T_UPDATE_DTO updateDto){
-        return entity;
+    default void updateFields(T entity, T_UPDATE_DTO updateDto) {
+        System.out.println("!! started !!");
+        Map<String, Field> updateFields = new HashMap<>();
+
+        for (Field f: updateDto.getClass().getDeclaredFields()){
+            updateFields.put(f.getName(), f);
+        }
+
+//        Set<String> entityFields = Arrays.stream(
+//                        entity.getClass().getDeclaredFields()
+//                )
+//                .map(Field::getName)
+//                .collect(Collectors.toSet());
+
+        System.out.println("update fields : " + updateFields);
+//        System.out.println("entity fields : " + entityFields);
+
+        for (Field field: entity.getClass().getDeclaredFields()) {
+            System.out.println("field : " + field);
+            if(!updateFields.containsKey(field.getName())) continue;
+            Field updateField = updateFields.get(field.getName());
+            updateField.setAccessible(true);
+            field.setAccessible(true);
+
+            try {
+                field.set(entity, updateField.get(updateDto));
+                System.out.println(updateField.getName() + " is updated");
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+//        for (String entityField :
+//                entityFields) {
+//            System.out.println("entityFields : " + entityField);
+//
+//            if(!updateFields.contains(entityField)) {
+//                System.out.println(entityField + " not matched");
+//                continue;
+//            }
+//            try {
+//                System.out.println("updating field " + entityField);
+//
+//                entity.getClass().getDeclaredField(entityField).setAccessible(true);
+//                updateDto.getClass().getDeclaredField(entityField).setAccessible(true);
+//
+//                System.out.println("data : " + updateDto.getClass().getDeclaredField(entityField).get(updateDto));
+//
+//                entity.getClass().getDeclaredField(entityField).set(entity, updateDto.getClass().getDeclaredField(entityField).get(updateDto));
+//            } catch (IllegalAccessException | NoSuchFieldException e) {
+//                logger.error(e.getMessage());
+//                logger.error("failed to update for " + getEntityType().getTypeName() + " on dto " + getUpdateEntityType());
+//                return;
+//            }
+//        }
+
+        System.out.println("!! ended !!");
+    }
+
+    default void beforeUpdate(T entity, T_UPDATE_DTO updateDto){
+        return;
     }
 }
